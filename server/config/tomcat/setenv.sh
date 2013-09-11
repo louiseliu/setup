1aea4f04236d204a36ab54ff58f2c441b524c66e
zxlsal <zxlsal@gmail.com>
2013-09-11 22:53:41 +0800

    1、开发平台的配置

--- server/config/tomcat/setenv.sh
+++ server/config/tomcat/setenv.sh
@@ -1,7 +1,7 @@
 JAVA_OPTS="$JAVA_OPTS -server -Xmn256m -Xms512m -Xmx512m -XX:NewSize=256m -XX:MaxNewSize=256m -XX:PermSize=256m -XX:MaxPermSize=256m"
 
-CATALINA_OPTS="$CATALINA_OPTS -DGERRIT_SITE=$HOME/gerrit"
+JAVA_OPTS="$JAVA_OPTS -Dgerrit.site_path=/var/gerrit"
+JAVA_OPTS="$JAVA_OPTS -DJENKINS_HOME=/var/jenkins"
+JAVA_OPTS="$JAVA_OPTS -Dplexus.nexus-work=/var/nexus"
 
 #CATALINA_OPTS="$CATALINA_OPTS -Xdebug -Xnoagent -Djava.compiler=NONE -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=8787"
-
-JAVA_OPTS="$JAVA_OPTS -Duser.home=$HOME/work"
